87f4340775ee84d7c12834f0ce624ce9abda181e
Ishan Chho <cargokhakis@gmail.com>
2017-11-12 15:16:05 -0700

    more cleaning

--- Titanic/script.py
+++ Titanic/script.py
@@ -1,75 +1,83 @@
-# Without looking at the data:
-# 'PassengerId', 'Survived', and 'Name' columns are used for identification, not training.
-# 'Pclass' and 'Fare' columns ('Cabin', 'Ticket') are likely to be highly correlated. Since we are using a decision tree as our first model, we will represent all of these using Pclass.
-# 'Embarked' locations should not affect survival.
-# Higher # of parents should increase child's survival, but higher # of children should decrease parents survival. Therefore, the 'Parch' column is unclear.
-# 'Ticket' and 'Cabin' could show clusters of survivers and casualties.
+'''
+1. 'PassengerId', 'Survived', and 'Name' columns are used for identification, and not training.
+2. 'Pclass', 'Fare', 'Cabin', and 'Ticket' columns are likely to show correlation, and since we are using a decision tree model, we will use 'Pclass' to represent all of them.
+3. The 'Embarked' column is logically irrelevant to a passanger's survival.
+4. A higher number of parents should increase a child's rate of survival, but a higher number of children should decreate a parent's rate of survival. Because of this contradiction, these columns are unreliable for now (we will still use them and see what we get).
 
-# Useful: ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']
-# Not Useful: ['PassengerId', 'Survived', 'Name', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+Useful: ['Pclass', 'Sex', 'Age', 'Sibsp', 'Parch']
+Not Useful: ['PassengerId', 'Survived', 'Name', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+'''
+
+
 
 import csv
 from sklearn import tree
 
-def importcols(file):
-    return list(csv.reader(open(file)))[0]
-def importdata(file):
-    l = list(csv.reader(open(file)))
-    del l[0]
-    return l
+def formatdata(data):
+    outdata = data
+    rawcols = outdata[0]
+    del outdata[0]
+    for item in outdata:
+        sex = item[rawcols.index('Sex')]
+        if sex == 'male':
+            sex = 1
+        elif sex == 'female':
+            sex = 0
+        item[rawcols.index('Sex')] = sex
+        for col in item:
+            if col == '':
+                outdata[outdata.index(item)][item.index(col)] = '0'
+    return outdata
 
-def train(input, output):
-    
-def predict(input):
-
-cols = importcols('train.csv')
-data = importdata('train.csv')
-
-cols = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']
-rawcols = ['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
-
-X = []
-Y = []
-
-for person in data:
-    if person[rawcols.index('Sex')] == 'male':
-        person[rawcols.index('Sex')] = 1
-    elif person[rawcols.index('Sex')] == 'female':
-        person[rawcols.index('Sex')] = 0
-    output = []
+def getcols(data, rawcols, cols):
+    outdata = data
+    colindex = []
     for col in cols:
-        if person[rawcols.index(col)] != '':
-            output.append(float(person[rawcols.index(col)]))
-        else:
-            output.append(0)
-    X.append(output)
-    Y.append(person[1])
+        colindex.append(rawcols.index(col))
+    colindex.sort(reverse=True)
+    for item in outdata:
+        length = len(item)
+        for i in range(length-1, -1, -1):
+            if i not in colindex:
+                del item[i]
+    return outdata
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, Y)
+def train(file, inCols, outCols):
+    rawfile = list(csv.reader(open(file)))
+    rawcols = rawfile[0]
+    data = formatdata(rawfile)
+#    X = getcols(data, rawcols, inCols)
+    X = getcols(data, rawcols, ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch'])
+    print(X)
+    Y = getcols(data, rawcols, outCols)
+    clf = tree.DecisionTreeClassifier()
+    return clf.fit(X, Y)
 
-test = importdata('test.csv')
-testX = []
-rawcols = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
-for person in test:
-    if person[rawcols.index('Sex')] == 'male':
-        person[rawcols.index('Sex')] = 1
-    elif person[rawcols.index('Sex')] == 'female':
-        person[rawcols.index('Sex')] = 0
-    output = []
-    for col in cols:
-        if person[importcols('test.csv').index(col)] != '':
-            output.append(person[importcols('test.csv').index(col)])
-        else:
-            output.append(0)
-    testX.append(output)
-    
-prediction = clf.predict(testX)
-print(len(prediction))
+def predict(infile, inCols, outfile, model, format):
+    rawfile = list(csv.reader(open(infile)))
+    rawcols = rawfile[0]
+    data = formatdata(rawfile)
+#    X = getcols(data, rawcols, inCols)
+    X = getcols(data, rawcols, ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch'])
+    prediction = model.predict(X)
+    print(prediction)
+#    with open(outfile, 'w') as file:
+#        header = ''
+#        for item in format:
+#            header += item + ','
+#        header = header[:-1]
+#        header += '\n'
+#        file.write(header)
+#        for item in range(len(prediction)):
+#            output = ''
+#            for col in item:
+#                output += 
+#            file.write(test[item][0] + ',' + prediction[item] + '\n')
 
-with open('prediction.csv', 'w') as file:
-    file.write('PassengerId,Survived\n')
-    for item in range(len(prediction)):
-        file.write(test[item][0] + ',' + prediction[item] + '\n')
+inCols = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']
+outCols = ['Survived']
+
+clf = train('train.csv', inCols, outCols)
+predict('test.csv', inCols, 'prediction.csv', clf, ['PassengerId', 'Survived'])
 
 # Prediction Score: 0.70813
