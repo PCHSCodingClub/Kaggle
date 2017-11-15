5410c6112248f2f7811f4ce933927a01681c2a78
Ishan Chho <cargokhakis@gmail.com>
2017-11-15 15:13:53 -0700

    added conference notes

---
+++ Titanic/notes.py
@@ -0,0 +1,19 @@
+#anbubin@microsoft.com
+
+#1. reccomendation
+#2. classification filters
+#3. fraud/cancer detection
+
+#Pandas
+#Matplotlib
+
+#Testing :R2, F1
+
+#Underfitting: oversimplification
+#Overvitting: memirizing data, not generalizing
+#	High testing error, low training error
+
+# pandas correlation detection
+
+#sklearn important features
+
